2683d5b6ccee8e926a9d67e306058ee9a6ac7221
Rawrhann <marcrohannmching@iskolarngbayan.pup.edu.ph>
2025-01-29 11:49:53 +0800

    Remove commented-out bubble sort implementation from SortingVisualizer component

--- src/pages/Sorting.jsx
+++ src/pages/Sorting.jsx
@@ -122,30 +122,6 @@ const SortingVisualizer = () => {
         setTimeout(() => setNotification(""), 3000);
     };
 
-    /*const bubbleSort = async () => {
-        setIsSorting(true);
-        let arr = array.slice();
-        let n = arr.length;
-        for (let i = 0; i < n - 1; i++) {
-            for (let j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    let temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-                setActiveIndex(j);
-                setArray([...arr]);
-                await new Promise((resolve) => setTimeout(resolve, delay));
-            }
-        }
-        setIsSorting(false);
-        setIsSorted(true);
-        setActiveIndex(null);
-        setNotification("Sorting Complete!");
-        setTimeout(() => setNotification(""), 3000);
-    };*/
-
-
     const bubbleSort = async () => {
         setIsSorting(true);
         let arr = array.slice();
